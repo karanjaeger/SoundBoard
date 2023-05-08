f94bdbcade40de6e0ee6a9a2ec15964485eb3b78
karanjaeger <114157430+karanjaeger@users.noreply.github.com>
2023-05-08 09:28:05 +0530

    Added initial Files

---
+++ index.js
@@ -0,0 +1,96 @@
+const doSong = document.getElementById("Do");
+const reSong = document.getElementById("Re");
+const miSong = document.getElementById("Mi");
+const faSong = document.getElementById("Fa");
+const saSong = document.getElementById("Sa");
+const laSong = document.getElementById("La");
+const tiSong = document.getElementById("Ti");
+const doOctave = document.getElementById("DO");
+
+function playDo()
+{
+    if (doSong.paused) {
+        doSong.currentTime = 0;
+        doSong.play();
+    }
+    else {
+        doSong.currentTime = 0;
+    }
+}
+
+function playRe() 
+{
+    if (reSong.paused) {
+        reSong.currentTime = 0;
+        reSong.play()
+    }
+    else {
+        reSong.currentTime = 0;
+    }
+}
+
+function playMi() 
+{
+    if (miSong.paused) {
+        miSong.currentTime = 0;
+        miSong.play()
+    }
+    else {
+        miSong.currentTime = 0;
+    }
+}
+
+function playFa() 
+{
+    if (faSong.paused) {
+        faSong.currentTime = 0;
+        faSong.play()
+    }
+    else {
+        faSong.currentTime = 0;
+    }
+}
+
+function playSa() 
+{
+    if (saSong.paused) {
+        saSong.currentTime = 0;
+        saSong.play()
+    }
+    else {
+        saSong.currentTime = 0;
+    }
+}
+
+function playLa() 
+{
+    if (laSong.paused) {
+        laSong.currentTime = 0;
+        laSong.play()
+    }
+    else {
+        laSong.currentTime = 0;
+    }
+}
+
+function playTi()
+{
+    if (tiSong.paused) {
+        tiSong.currentTime = 0;
+        tiSong.play()
+    }
+    else {
+        tiSong.currentTime = 0;
+    }
+}
+
+function playDO() 
+{
+    if (doOctave.paused) {
+        doOctave.currentTime = 0;
+        doOctave.play()
+    }
+    else {
+        doOctave.currentTime = 0;
+    }
+}
